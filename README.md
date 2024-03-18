6b26135254702cc75296f4fdd9b3e21bba9ca5ad
ALAHMEDF <ahmedfejjari13@gmail.com>
2024-03-18 15:50:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+1- npm install 
+2- cd backend 
+3- npm start 
+4- cd frontend 
+5- npm start 
---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'iphone12',
+      slug: 'iphone12',
+      category: 'phones',
+      image: '/images/p1.jpg', // 679px × 829px
+      price: 420,
+      countInStock: 10,
+      brand: 'apple',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'neuf',
+    },
+    {
+      name: 'samsung a51',
+      slug: 'a51',
+      category: 'phones',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'samsung',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'état correct',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'Adidas Fit Pant',
+      slug: 'adidas-fit-pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    
+  ],
+};
+export default data;
